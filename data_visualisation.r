7d0a52467cc76890ee8b98fb9c1ad02a7e599dee
Nikita Muravytskyi <nick.muravytskyi@gmail.com>
2022-12-27 14:01:55 +0100

    added graph with average no a scale of a week

--- data_visualisation.r
+++ data_visualisation.r
@@ -4,6 +4,9 @@ library(dplyr)
 library(shiny)
 library(lubridate)
 
+# week starts on Monday
+options("lubridate.week.start" = 1)
+
 format_si <- function(...) {
   # Based on code by Ben Tupper
   # https://stat.ethz.ch/pipermail/r-help/2012-January/299804.html
@@ -40,19 +43,37 @@ data <- read_csv("data/samples_5m_subset_v1.csv",
 subset <- subset(data, timestamp >= "2022-03-01") %>%
       mutate(diff = incoming_rate_avg - lag(incoming_rate_avg))
 
-subset_by_day <- subset %>%
-            mutate(day = floor_date(timestamp, "day")) %>%
-            mutate(link = paste(src_host, "...", dst_host)) %>%
-            group_by(day, link) %>%
-            summarize(avg = mean(incoming_rate_avg),
-                      src_host = src_host,
-                      dst_host = dst_host)
+d <- subset %>%
+  mutate(day = floor_date(timestamp, "day")) %>%
+  # TODO: maybe move these to the ggplots
+  mutate(week_start = floor_date(timestamp, "week")) %>%
+  mutate(week_start_delta = timestamp - week_start) %>%
+  mutate(link = paste(src_host, "...", dst_host)) %>%
+  mutate(week = week(timestamp)) %>%
+  mutate(wday = wday(day, week_start = 1, label = TRUE, abbr = FALSE))
 
-gg <- ggplot(subset_by_day, aes(x = day, y = avg, colour = link)) +
-        geom_line() +
-        scale_y_continuous(labels = format_si()) +
-        ylab("bitrate") +
-        xlab("time") +
-        labs(title = "Bitrate statistics averaged by day")
+gg <- d %>%
+  group_by(day, link) %>%
+  summarize(avg = mean(incoming_rate_avg),
+            src_host = src_host,
+            dst_host = dst_host) %>%
+  ggplot(aes(x = day, y = avg, colour = link)) +
+    geom_line() +
+    scale_y_continuous(labels = format_si()) +
+    ylab("bitrate") +
+    xlab("time") +
+    labs(title = "Bitrate statistics averaged by day")
+
+show(gg)
+
+gg <- d %>%
+  group_by(week_start_delta, link) %>%
+  summarize(avg = mean(incoming_rate_avg)) %>%
+  ggplot(aes(x = week_start_delta, y = avg, colour = link)) +
+    geom_line() +
+    scale_y_continuous(labels = format_si()) +
+    ylab("bitrate") +
+    xlab("time") +
+    labs(title = "Bitrate statistics averaged by day of the week")
 
 plot(gg)
