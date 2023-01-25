242d14d23687c2e54cdfb80b9234585d76a0e3be
Nikita Muravytskyi <nick.muravytskyi@gmail.com>
2023-01-25 11:56:16 +0100

    baseline model - RNN

--- metadata.py
+++ metadata.py
@@ -10,6 +10,28 @@ ADJACENCY_MATRIX = [
     [0, 0, 0, 0, 0, 0],
 ]
 
+CONNECTIONS = [
+    # B1
+    ("b1", "ftj"),
+    ("b1", "uci"),
+    ("b1", "b6"),
+    # B6
+    ("b6", "ms"),
+    ("b6", "uci"),
+    ("b6", "b1"),
+    ("b6", "ftj"),
+    # UCI
+    ("uci", "ftj"),
+    ("uci", "b1"),
+    ("uci", "b6"),
+    ("uci", "cyfronet"),
+     # FTJ
+    ("ftj", "b1"),
+    ("ftj", "uci"),
+    ("ftj", "b6"),
+    ("ftj", "cyfronet"),
+    ]
+
 NODE_IDS_TO_LABELS_MAPPING = {
     0: "cyfronet",
     1: "uci",
