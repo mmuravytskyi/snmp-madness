019b64da9803f022a4dc842f67114c53c1e2ba8d
Nikita Muravytskyi <nick.muravytskyi@gmail.com>
2023-01-27 16:59:13 +0100

    added learning and evaluation plots

--- utils.py
+++ utils.py
@@ -31,10 +31,11 @@ def draw_jraph_graph_structure(jraph_graph: jraph.GraphsTuple) -> None:
       labels=metadata.NODE_IDS_TO_LABELS_MAPPING
       )
 
-def plot_samples(truth, prediction):
+def plot_samples(truth, prediction,
+                 truth_label='truth', prediction_label='predicted'):
   assert truth.shape == prediction.shape
-  df = pd.DataFrame({'truth': truth.squeeze(), 'predicted': prediction.squeeze()}).reset_index()
-  df = pd.melt(df, id_vars=['index'], value_vars=['truth', 'predicted'])
+  df = pd.DataFrame({truth_label: truth.squeeze(), prediction_label: prediction.squeeze()}).reset_index()
+  df = pd.melt(df, id_vars=['index'], value_vars=[truth_label, prediction_label])
   plot = (
       gg.ggplot(df)
       + gg.aes(x='index', y='value', color='variable')
